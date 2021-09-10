f2795eb824397c851b88a08861cb2f91484846c9
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-10 16:05:31 -0300

    Add files via upload

---
+++ calculadora ad, sub, mul, div/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+  namespace myapp
+  {
+      class Program3
+      {
+          static void Main(string[] args)
+          {
+             
+             string op;
+             float num1,num2,resultado;
+
+             Console.Write("informe o primeiro número: ");
+             num1 = float.Parse(Console.ReadLine());
+             Console.Write("informe o segundo número: ");
+             num2 = float.Parse(Console.ReadLine());
+             Console.WriteLine("Qual operação você deseja: adição, subtração, multiplicação ou divisão? ");
+             op = Console.ReadLine();
+
+             switch (op)
+             {
+                 case "adição":
+                 resultado = num1 + num2;
+                 Console.Write("O esultado da adição é: " + resultado);
+                 break;
+
+                 case "subtração":
+                 resultado = num1 - num2;
+                 Console.Write("O esultado da subtração é: " + resultado);
+                 break;
+
+                 case "multiplicação":
+                 resultado = num1 * num2;
+                 Console.Write("O esultado da multiplicação é: " + resultado);
+                 break;
+
+                 case "divisão":
+                 resultado = num1 / num2;
+                 Console.Write("O esultado da divisão é: " + resultado);
+                 break;
+
+                 default:
+                 Console.Write("operação inválida.");
+                 break;
+             }
+          }    
+      }   
+  }    
